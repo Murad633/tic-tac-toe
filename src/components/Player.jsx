1cbaf0ddfebb1da92c79e9e76caefe5bdbf6fb75
Murad Israfilov <muradisrafilov542@gmail.com>
2024-10-05 17:56:04 +0400

    Player component optimized

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,19 +1,25 @@
 import { useState } from "react";
 
-export const Player = ({ initialName, symbol, isActive }) => {
-    const [playerName, setPlayerName] = useState(initialName)
+export const Player = ({ initialName, symbol, isActive, onChangeName }) => {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
   const handleClick = () => {
-    setIsEditing((editing)=>!editing);
+    setIsEditing((editing) => !editing);
+    if (isEditing) onChangeName(symbol, playerName);
   };
   const handleChange = (e) => {
-    setPlayerName(e.target.value)
-  }
+    setPlayerName(e.target.value);
+  };
   return (
-    <li className={isActive ? 'active' : undefined}>
-      <span className="player" >
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
         {isEditing ? (
-          <input type="text" value={playerName} onChange={(e) => handleChange(e)} required />
+          <input
+            type="text"
+            value={playerName}
+            onChange={(e) => handleChange(e)}
+            required
+          />
         ) : (
           <span className="player-name">{playerName}</span>
         )}
